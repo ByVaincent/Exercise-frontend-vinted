8020ffde7310bda5cd723df8d01b5eabcff9f386
Vincent Jacquelin <vjacquelinpro@gmail.com>
2025-03-18 10:26:32 +0100

    css publish form ok

--- src/pages/Publish.jsx
+++ src/pages/Publish.jsx
@@ -2,6 +2,7 @@ import { Navigate } from "react-router-dom";
 import { useState } from "react";
 import axios from "axios";
 import Dropzone from "../components/DropZone";
+import Button from "../components/Button/Button";
 
 const Publish = ({ token }) => {
   //   const [pictures, setPictures] = useState(null);
@@ -76,94 +77,117 @@ const Publish = ({ token }) => {
           }}
         /> */}
         <fieldset>
-          <label htmlFor="title">Titre</label>
+
+          <div> <label htmlFor="title">Titre</label>
           <input
             type="text"
             name="title"
             id="title"
+            placeholder="ex: Chemise Sézanne verte"
             value={publishForm.title}
             onChange={(event) => {
               updateState(event, "title", publishForm, setPublishForm);
             }}
-          />
-          <label htmlFor="description">Décris ton article</label>
+          /></div>
+         <div>
+           <label htmlFor="description">Décris ton article</label>
           <input
             type="text"
             name="description"
             id="description"
+            placeholder="ex: Très peu porté"
             onChange={(event) => {
               updateState(event, "description", publishForm, setPublishForm);
             }}
           />
+         </div>
+         
         </fieldset>
 
         <fieldset>
-          <label htmlFor="brand">Marque</label>
+          <div>
+                    <label htmlFor="brand">Marque</label>
           <input
             type="text"
             name="brand"
             id="brand"
+            placeholder="ex: Zara"
             value={publishForm.brand}
             onChange={(event) => {
               updateState(event, "brand", publishForm, setPublishForm);
             }}
           />
-          <label htmlFor="size">Taille</label>
+          </div>
+            <div>
+              <label htmlFor="size">Taille</label>
           <input
             type="text"
             name="size"
             id="size"
+            placeholder="ex: L / 42 / 12"
             value={publishForm.size}
             onChange={(event) => {
               updateState(event, "size", publishForm, setPublishForm);
             }}
           />
-          <label htmlFor="color">Couleur</label>
+            </div>
+          <div>
+            <label htmlFor="color">Couleur</label>
           <input
             type="text"
             name="color"
             id="color"
+            placeholder="ex: Fushia"
             value={publishForm.color}
             onChange={(event) => {
               updateState(event, "color", publishForm, setPublishForm);
             }}
           />
-          <label htmlFor="condition">État</label>
+          </div>
+          <div><label htmlFor="condition">État</label>
           <input
             type="text"
             name="condition"
             id="condition"
+            placeholder="Neuf avec étiquette"
             value={publishForm.condition}
             onChange={(event) => {
               updateState(event, "condition", publishForm, setPublishForm);
             }}
-          />
-          <label htmlFor="city">Lieu</label>
+          /></div>
+          <div><label htmlFor="city">Lieu</label>
           <input
             type="text"
             name="city"
             id="city"
+            placeholder="ex: Paris"
             value={publishForm.city}
             onChange={(event) => {
               updateState(event, "city", publishForm, setPublishForm);
             }}
-          />
+          /></div>
+          
         </fieldset>
 
         <fieldset>
-          <label htmlFor="price">Prix</label>
+          <div>
+                    <label htmlFor="price">Prix</label>
           <input
             type="text"
             name="price"
             id="price"
+            placeholder="0.00 €"
             value={publishForm.price}
             onChange={(event) => {
               updateState(event, "price", publishForm, setPublishForm);
             }}
           />
+          </div>
+  
         </fieldset>
 
-        <input type="submit" value={"Ajouter"} />
+            <Button text={"Ajouter"} classProps={"button-sale"}/>
+        {/* <input type="submit" value={"Ajouter"} /> */}
       </form>
     </main>
   );
